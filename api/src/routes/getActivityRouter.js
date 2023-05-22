e501ba4158ccd3b4798023dedd964ce346aeb3f8
Nicolas Campos <nikovic123@gmail.com>
2023-05-22 20:45:38 -0300

    terminado, rediseñar css

--- api/src/routes/getActivityRouter.js
+++ api/src/routes/getActivityRouter.js
@@ -5,8 +5,8 @@ const cts = require("../controllers/activityCtrl");
 getRouter.use("/", async(req, res) => {
     try {
         const getActivities = await cts.getActivities();
-        console.log(getActivities);
-       getActivities.length ? res.status(200).json(getActivities) : res.status(404).json({error: "activities not found"})
+        //console.log(getActivities);
+        getActivities.length ? res.status(200).json(getActivities) : res.status(404).json({error: "activities not found"})
     } catch (error) {
         res.status(500).json({error: error.message})
     }
